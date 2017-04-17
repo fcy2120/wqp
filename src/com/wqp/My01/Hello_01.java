ee41956454d1dc29aa681b6533d8965ee5cbfc27
fu2120 <51432158@qq.com>
2017-04-17 12:45:49 +0800

    1

---
+++ src/com/wqp/My01/Hello_01.java
@@ -0,0 +1,10 @@
+package com.wqp.My01;
+
+public class Hello_01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/wqp/HelloWWW.java
@@ -0,0 +1,10 @@
+package wqp;
+
+public class HelloWWW {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("ÐÂµÄ");
+	}
+
+}
